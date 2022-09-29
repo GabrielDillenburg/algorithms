da6922cf89707bb11cbba92e56f1cc3741c7d8b0
Gabriel Dillenburg <gabriel.dillenburg@ibm.com>
2022-09-28 22:13:51 -0300

    add baseball game challenge

--- Algos/baseballGame.js
+++ Algos/baseballGame.js
@@ -6,26 +6,27 @@
   const array = []
   for (let i = 0; i < operations.length; i++) {
     const value = operations[i]
-    const previous = operations[i-1]
-    const previous2 = operations[i-2]
+    const previous = array[array.length-1]
+    const previous2 = array[array.length-2]
     
     if (value == '+') {
-      const item = parseInt(previous) + parseInt(previous2)
+      const item = previous + previous2
       array.push(item)
     } else if (value == 'D') {
-      const item = 2 * parseInt(previous)
+      const item = 2 * previous
       array.push(item)
     } else if (value == 'C') {
       array.pop()
     } else {
       const item = parseInt(value)
       array.push(item)
-    }
-    
-    return array
-    
+    }    
   }
-    
+  const res =  array.reduce((acc, value) => {
+    return acc + value
+  }, 0)
+  
+  return res
 };
 
 const ops = ["5","2","C","D","+"]
